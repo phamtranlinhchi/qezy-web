b6a2b216d7a5f07a1f736a56427d1481e2edbb3b
Linh Chi <admin@admins-MacBook-Pro.local>
2024-05-28 02:09:38 +0700

    Remove isAdmin middleware

--- src/components/WelcomeUser/index.tsx
+++ src/components/WelcomeUser/index.tsx
@@ -6,12 +6,6 @@ import { Box, Button, FormControl, IconButton, InputAdornment, InputLabel, Outli
 // import MicrosoftAuth from '../MicrosoftAuth';
 import { Link, Navigate, useNavigate } from 'react-router-dom';
 import { useState } from "react";
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import Cookies from "js-cookie"
-
-import { login } from "../../helpers/fetch";
-
 
 
 const WelcomeUser = () => {
@@ -20,22 +14,33 @@ const WelcomeUser = () => {
   return (
     <Box sx={{
       width: "100vw",
-      height: "100vh",
+      height: "80vh",
       display: "flex",
       justifyContent: "center",
       alignItems: "center"
     }}>
-      <TextField
-        label='Exam ID Here'
-        value={examId}
-        onChange={(e) => setExamId(e.target.value)}
-        size="small"
-        sx={{
-          marginRight: "16px",
-          width: "300px"
-        }}
-      />
-      <a href={`http://localhost:8082/${examId}`}><Button variant="contained" color="success">Go to exam</Button></a>
+      <Box>
+        <Box sx={{ width: "100%", textAlign: "center", marginBottom: "20px" }}>
+          <img
+            src="../public/assets/images/qezy_logo.png"
+            alt=""
+            style={{ height: '130px' }}
+          ></img>
+        </Box>
+        <Box>
+          <TextField
+            label='Exam ID Here'
+            value={examId}
+            onChange={(e) => setExamId(e.target.value)}
+            size="small"
+            sx={{
+              marginRight: "16px",
+              width: "300px"
+            }}
+          />
+          <a href={`http://localhost:8082/${examId}`}><Button variant="contained" color="success">Go to exam</Button></a>
+        </Box>
+      </Box>
     </Box>
   );
 };
